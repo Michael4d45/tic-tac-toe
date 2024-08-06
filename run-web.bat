a426047b53488539a2039047dad63375d238566a
Michael Harvey <michael4d45@gmail.com>
2024-08-05 20:48:29 -0600

    fix bat

--- run-web.bat
+++ run-web.bat
@@ -1,1 +1,1 @@
-npx local-web-server   --https   --cors.embedder-policy "require-corp"  --cors.opener-policy "same-origin"   --directory builds/html5
+npx local-web-server   --https   --cors.embedder-policy "require-corp"  --cors.opener-policy "same-origin"   --directory build/html5
